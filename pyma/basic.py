46f691481ada1d27743f4ec743411f6deff8ef08
wsnk <wsnk00@yandex.com>
2025-02-09 00:46:26 +0200

    Basic unit tests (#3)

---
+++ pyma/basic.py
@@ -0,0 +1,49 @@
+from pyma.matchers import Matcher, Mismatch, _match
+
+
+class DictContains(Matcher):
+    """ Matches, if a dict contains expected subset of key-value pairs
+    """
+
+    def __init__(self, expected, *, recursive=True):
+        self.expected = expected
+        self.recursive = recursive
+
+    def __repr__(self):
+        return f"DictContains({repr(self.expected)})"
+
+    def _match(self, other):
+        for k, val in self.expected.items():
+            if k not in other:
+                raise Mismatch(other, k, f"key '{k}' not found")
+            actual_val = other[k]
+            try:
+                if self.recursive and isinstance(val, dict):
+                    val = DictContains(val)
+                _match(val, actual_val)
+            except Mismatch as e:
+                raise e.prepend(k)
+
+
+class InInterval:
+    # The matcher matches, if a given value (other) lies in the interval
+
+    def __init__(self, *value):
+        self.first_value, self.second_value = value
+
+    def __eq__(self, other):
+        return self.first_value <= other <= self.second_value
+
+    def __repr__(self):
+        return f"{self.first_value}, {self.second_value}"
+
+
+class IsType:
+    def __init__(self, *types):
+        self.types = types
+
+    def __eq__(self, other):
+        return isinstance(other, self.types)
+
+    def __repr__(self):
+        return ', '.join(it.__name__ for it in self.types)
---
+++ tests/test_basic.py
@@ -0,0 +1,43 @@
+import pytest
+from pyma.basic import InInterval, IsType, DictContains
+
+
+def test_ininterval():
+    assert 7 == InInterval(1, 7)
+    assert 10 != InInterval(1, 7)
+
+
+def test_istype():
+    assert 2 != IsType(str)
+    assert 2 == IsType(int, str)
+    assert "b" == IsType(int, str)
+    assert "b" != IsType(int)
+    assert "b" == IsType(str)
+
+
+def test_dict__plain_match():
+    assert {"a": 1, "b": 2} == DictContains({"a": 1, "b": 2})
+    assert {"a": 1, "b": 2} == DictContains({"a": 1})
+    assert {"a": 1, "b": 2} == DictContains({"b": 2})
+    assert {"a": 1, "b": 2} == DictContains({})
+
+
+@pytest.mark.parametrize("actual, expected, message", [
+    ({"a": 1, "b": 2}, {"a": 1, "b": 3}, "Value 2 at 'b' does not match - 2 != 3"),
+    ({"a": 1}, {"b": 1}, "Value {'a': 1} at 'b' does not match - key 'b' not found")
+])
+def test_dict__plain_mismatch(actual, expected, message):
+    with pytest.raises(AssertionError, match=f"\n *{message}$"):
+        assert actual == DictContains(expected)
+
+
+@pytest.mark.parametrize("actual, expected, message", [
+    (
+        {"a": 1, "b": {"c": {"d": 2, "e": 3}}},
+        {"b": {"c": {"e": 4}}},
+        "Value 3 at 'b.c.e' does not match - 3 != 4"
+    ),
+])
+def test_dict__nested_mismatch(actual, expected, message):
+    with pytest.raises(AssertionError, match=f"\n *{message}$"):
+        assert actual == DictContains(expected)
